34163b749188992d014abfb337154ae0c0f114cd
larry659 <larryfunmbi26@gmail.com>
2022-02-28 13:38:31 +0100

    adding new project as repository

---
+++ src/main/java/com/example/EuroSpace/EuroSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.EuroSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EuroSpace/EuroSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EuroSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
